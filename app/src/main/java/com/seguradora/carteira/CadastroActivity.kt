093a8b278533d042b6ffadb105cef563fd915c86
luizaandradeti <197419153+luizaandradeti@users.noreply.github.com>
2025-03-22 17:44:08 -0300

    binding

--- app/src/main/java/com/seguradora/carteira/CadastroActivity.kt
+++ app/src/main/java/com/seguradora/carteira/CadastroActivity.kt
@@ -6,8 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        TODO("TASK")
+        //binding lateint
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        TODO("TASK")
+        //binding inflate
+        //set conytent view
         setContentView(R.layout.activity_cadastro)
 
     }
